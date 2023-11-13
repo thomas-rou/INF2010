8cd5c74b44f8bb28be0f71d405e5d89841a58ec0
ThomasR <thomas.rouleau@polymtl.ca>
2023-11-12 22:00:37 -0500

    Partie 2 : Rapport et finalisation des tests

--- TP4/src/test/java/Partie2Test.java
+++ TP4/src/test/java/Partie2Test.java
@@ -1,5 +1,7 @@
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.sun.jdi.ArrayReference;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,22 +24,33 @@ import java.util.Collections;
 
 public class Partie2Test {
     private BinarySearchTree<Integer> binaryTree;
+    private BinarySearchTree<Integer> worstBinaryTree;
     private PriorityQueue<Integer> minHeap;
     private PriorityQueue<Integer> maxHeap;
 
     @BeforeEach
     public void setUp(){
         binaryTree = new BinarySearchTree<>();
+        worstBinaryTree = new BinarySearchTree<>();
         minHeap = new PriorityQueue<>();
         maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
 
+        ArrayList<Integer> inOrderList = new ArrayList<Integer>();
         ArrayList<Integer> shuffleList = new ArrayList<Integer>();
-        int numberOfValues = 10000;
+        int normalCaseNumberOfValues = 10000;
+        int worstCaseNumberOfValues = 5000;
 
-        for (int i = 0; i < numberOfValues; i++){
+        for (int i = 0; i < worstCaseNumberOfValues; i++){
+            inOrderList.add(i);
+        }
+        for (int i = 0; i < normalCaseNumberOfValues; i++){
             shuffleList.add(i);
         }
         Collections.shuffle(shuffleList);
+
+        for (Integer element : inOrderList){
+            worstBinaryTree.add(element);
+        }
 
         for (Integer element : shuffleList) {
             binaryTree.add(element);
@@ -78,46 +91,102 @@ public class Partie2Test {
     }
 
     @Test
-    public void testSuppressionNormalCase(){
-        /*
-        ArrayList<Integer> suppressionShuffleList = new ArrayList<Integer>();
-        int suppresionNumberOfValues = 10000;
+    public void testInsertionWorstCase(){
+        ArrayList<Integer> insertionShuffleList = new ArrayList<Integer>();
+        int insertionNumberOfValues = 20000;
 
-        for (int i = 0; i < suppresionNumberOfValues; i++){
-            suppressionShuffleList.add(i);
+        for (int i = 10000; i < insertionNumberOfValues; i++){
+            insertionShuffleList.add(i);
         }
-        Collections.shuffle(suppressionShuffleList);
 
         long bstStartTime = System.nanoTime();
-        for (Integer element : suppressionShuffleList) {
-            binaryTree.remove(element);
+        for (Integer element : insertionShuffleList) {
+            worstBinaryTree.add(element);
         }
         long bstEndTime = System.nanoTime();
 
         long minHeapStartTime = System.nanoTime();
-        for (Integer element : suppressionShuffleList) {
-            minHeap.
+        for (Integer element : insertionShuffleList) {
+            minHeap.offer(element);
         }
         long minHeapEndTime = System.nanoTime();
 
         long maxHeapStartTime = System.nanoTime();
-        for (Integer element : suppressionShuffleList) {
+        for (Integer element : insertionShuffleList) {
             maxHeap.offer(element);
         }
         long maxHeapEndTime = System.nanoTime();
 
-        System.out.println("Temps d'insertion cas normal arbre BST: " + (bstEndTime - bstStartTime) + " ns, min-Heap:" + (minHeapEndTime - minHeapStartTime) + " ns, " + "max-Heap: " + (maxHeapEndTime - maxHeapStartTime) + " ns");
-         */
+        System.out.println("Temps d'insertion pire cas arbre BST: " + (bstEndTime - bstStartTime) + " ns, min-Heap:" + (minHeapEndTime - minHeapStartTime) + " ns, " + "max-Heap: " + (maxHeapEndTime - maxHeapStartTime) + " ns");
+    }
+
+    @Test
+    public void testSuppressionWorstCase(){
+        ArrayList<Integer> suppressionList = new ArrayList<Integer>();
+        int suppressionNumberOfValues = 10000;
+
+        for (int i = suppressionNumberOfValues; i >= 0; i--) {
+            suppressionList.add(i);
+        }
+
+        long bstStartTime = System.nanoTime();
+        for (Integer element : suppressionList) {
+           worstBinaryTree.remove(element);
+        }
+        long bstEndTime = System.nanoTime();
+
+        long minHeapStartTime = System.nanoTime();
+        for (Integer element : suppressionList) {
+            minHeap.poll();
+        }
+        long minHeapEndTime = System.nanoTime();
+
+        long maxHeapStartTime = System.nanoTime();
+        for (Integer element : suppressionList) {
+            maxHeap.poll();
+        }
+        long maxHeapEndTime = System.nanoTime();
+
+        //System.out.println("vérification si vide max-heap : " + maxHeap.size() + "min-heap : " + minHeap.size());
+        System.out.println("Temps de suppression pire cas arbre BST: " + (bstEndTime - bstStartTime) + " ns, min-Heap:" + (minHeapEndTime - minHeapStartTime) + " ns, " + "max-Heap: " + (maxHeapEndTime - maxHeapStartTime) + " ns");
     }
 
     @Test
     public void testGetMin(){
+        int getMinIterations = 10000;
 
+        long bstStartTime = System.nanoTime();
+        for (int i = 0; i < getMinIterations; i++) {
+            binaryTree.findMin(binaryTree.root);
+        }
+        long bstEndTime = System.nanoTime();
+
+        long minHeapStartTime = System.nanoTime();
+        for (int i = 0; i < getMinIterations; i++) {
+            minHeap.peek();
+        }
+        long minHeapEndTime = System.nanoTime();
+
+        System.out.println("Temps de recuperation du minimum arbre BST: " + (bstEndTime - bstStartTime + " ns, min-Heap:" + (minHeapEndTime - minHeapStartTime) + " ns"));
     }
 
     @Test
     void testGetMax(){
+        int getMaxIterations = 10000;
 
+        long bstStartTime = System.nanoTime();
+        for (int i = 0; i < getMaxIterations; i++) {
+            binaryTree.findMax(binaryTree.root);
+        }
+        long bstEndTime = System.nanoTime();
+
+        long maxHeapStartTime = System.nanoTime();
+        for (int i = 0; i < getMaxIterations; i++) {
+            maxHeap.peek();
+        }
+        long maxHeapEndTime = System.nanoTime();
+
+        System.out.println("Temps de recuperation du maximum arbre BST: " + (bstEndTime - bstStartTime + " ns, max-Heap:" + (maxHeapEndTime - maxHeapStartTime) + " ns"));
     }
 
 }
