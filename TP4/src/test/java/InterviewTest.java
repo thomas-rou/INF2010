0aae2089df5828cb7e0e33be16ab1b5fafc6b865
ThomasR <thomas.rouleau@polymtl.ca>
2023-11-12 22:04:56 -0500

    interviewTest.java : fix de la série de test
    Fix tel que mentionnée dans la section annonce du Discord du cours :
    Petit fix (ligne 7 du fichier interviewTest) :
    private Interview solution = new Interview();

--- TP4/src/test/java/InterviewTest.java
+++ TP4/src/test/java/InterviewTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InterviewTest {
-    private Interview solution;
+    private Interview solution = new Interview();
     private BinarySearchTree<Integer> tree;
     // Partie 3.1
 
